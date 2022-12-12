eba35eee44800301824660615460a80cc871a84b
y3n <y3ndev@gmail.com>
2022-12-12 21:42:59 +0100

    time example with set_time set_config_reg and get_time

--- examples/MAX31329_Time/MAX31329_Time.ino
+++ examples/MAX31329_Time/MAX31329_Time.ino
@@ -4,6 +4,36 @@ Max31329 rtc(&Wire);
 
 void setup() {
   Serial.begin(115200);
+  Wire.begin(26,27);
+
+  delay(200);
+
+  rtc.reset();
+
+  // setting config
+  max31329_config_t config;
+  config.config1 = 0x01;
+  config.config2 = 0x0;
+  uint16_t ret = rtc.set_config_reg(config);
+  Serial.print("config update:");
+  Serial.println(ret);
+
+  delay(1);
+
+  // setting time and date
+  max31329_time_t time;
+  max31329_calendar_t date;
+  
+  time.hours=21;
+  time.minutes=35;
+  time.seconds=0;
+  rtc.set_time(time);
+  date.day=1;
+  date.date=12;
+  date.month=12;
+  date.year=22;
+  rtc.set_calendar(date);
+  delay(1000);
 }
 
 void print_current_date_time(){
@@ -15,6 +45,17 @@ void print_current_date_time(){
     Serial.println("ERROR: Cannot get time.");
     return;
   }
+
+  max31329_config_t config;
+  if(rtc.get_config_reg(&config)) {
+    Serial.println("ERROR: Cannot get config.");
+    return;
+  }
+
+  // print config
+  Serial.print("RTC Config: ");
+  Serial.print(config.config1, BIN);
+  Serial.println(config.config2, BIN);
 
   Serial.print("RTC Time: ");
 
@@ -31,13 +72,6 @@ void print_current_date_time(){
   Serial.print(time.minutes);
   Serial.print(":");
   Serial.println(time.seconds);
-
-  max31329_config_t config_regs;
-  
-
-
-  Serial.print("RTC Config: ");
-  Serial.println();
 }
 
 void loop() {
